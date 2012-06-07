f59241968371c57682b4bb0870002816fbe8c114
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2012-06-07 16:24:37 +0200

    Spartan: build kernel in Genode's build-directory
    * Don't pollute the HelenOS source tree anymore
    * Build the kernel in 'kernel' sub-directory of Genode's build-directory
    * ignore contrib-files in git

--- base-spartan/src/kernel/x86/target.mk
+++ base-spartan/src/kernel/x86/target.mk
@@ -1,52 +1,20 @@
-DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+TARGET      = kernel
+CONTRIB_DIR = $(REP_DIR)/contrib/
+KBUILD_DIR  = $(BUILD_BASE_DIR)/kernel
+SPARTAN     = $(KBUILD_DIR)/kernel/kernel.bin
 
-CONTRIB_DIR := $(REP_DIR)/contrib/
+$(TARGET): $(SPARTAN)
 
-CONFIG_MAKEFILE = $(DIR)/Makefile.config
-CONFIG_HEADER = $(DIR)/config.h
+$(SPARTAN): $(KBUILD_DIR)/Makefile.common
+	$(MAKE) $(VERBOSE_DIR) -C $(CONTRIB_DIR)/kernel O=$(KBUILD_DIR)
+	$(VERBOSE)ln -sf $@ $(BUILD_BASE_DIR)/bin/$(TARGET)
 
-all: config autotool
-	$(MAKE) $(VERBOSE_DIR) -C $(CONTRIB_DIR)/kernel/ \
-		VPATH=$(CONTRIB_DIR)/kernel
-	$(MAKE) -C $(CONTRIB_DIR)/uspace/lib/c $(INCLUDE_ABI) $(INCLUDE_LIBARCH)
-	cp $(CONTRIB_DIR)/kernel/kernel.bin bin/kernel
+$(KBUILD_DIR)/Makefile.common: $(KBUILD_DIR)/config.h $(KBUILD_DIR)/Makefile.config
+	$(MAKE) -C $(CONTRIB_DIR) O=$(KBUILD_DIR) autotool
 
-autotool:
-	$(MAKE) $(VERBOSE_DIR) -C $(CONTRIB_DIR) \
-		VPATH=$(DIR) \
-		autotool
+$(KBUILD_DIR)/%: $(PRG_DIR)/%
+	$(VERBOSE)mkdir -p $(KBUILD_DIR)
+	$(VERBOSE)cp $< $@
 
-config:
-	cp $(CONFIG_MAKEFILE) $(CONTRIB_DIR)
-	cp $(CONFIG_HEADER) $(CONTRIB_DIR)
-
-#	$(MAKE) $(VERBOSE_DIR) -f $(CONTRIB_DIR)/Makefile \
-		-I $(DIR) \
-		VPATH=$(CONTRIB_DIR) \
-		autotool
-
-#	$(MAKE) $(VERBOSE_DIR) -C ./ -f $(CONTRIB_DIR)/Makefile \
-		VPATH=$(CONTRIB_DIR) \
-		config
-#	$(MAKE) $(VERBOSE_DIR) -f $(CONTRIB_DIR)/Makefile \
-		VPATH=$(CONTRIB_DIR) \
-		autotool
-
-#	$(MAKE) $(VERBOSE_DIR)  $(CONTRIB_DIR)/kernel/Makefile \
-		-I $(CONTRIB_DIR)/kernel \
-		VPATH=$(CONTRIB_DIR)/kernel
-
-#include $(REP_DIR)/contrib/Makefile
-
-#TARGET			= kernel
-#SPARTAN_SRC_DIR		= $(REP_DIR)/contrib/kernel
-#SPARTAN_BUILD_DIR	= $(BUILD_BASE_DIR)/kernel
-#REQUIRES 		+= spartan
-#LIBS			= kernel
-
-#SRC_CC   = hello.cc \
-	$(REP_DIR)/contrib/uspace/lib/c/arch/$(UARCH)/src/syscall.S
-#INC_DIR += $(REP_DIR)/contrib/uspace/lib/c/include
-
-#figure out where to add the syscall.S from $(SPARTAN_LIBC_DIR)/arch/$(UARCH)/src/syscall.S !
-
+clean cleanall:
+	$(VERBOSE)rm -rf $(KBUILD_DIR) bin/kernel
